a95467ca120e2715f81f4b834301b6399c77f6b4
csmithfiuba <csmith@fi.uba.ar>
2024-04-23 16:57:11 -0300

    Initial Message Broker

--- src/connect-flags.rs
+++ src/connect-flags.rs
@@ -2,7 +2,7 @@
 struct ConnectFlags {
     clean_session: bool,
     will_flag: bool,
-    will_qos: u8, // Puede ser 0, 1, o 2
+    will_qos: u8,
     will_retain: bool,
     user_name_flag: bool,
     password_flag: bool,
@@ -24,23 +24,23 @@ impl ConnectFlags {
         let mut flags_byte: u8 = 0;
 
         if self.clean_session {
-            flags_byte |= 0b0000_0010; // Setea el bit 1 (Clean Session)
+            flags_byte |= 0b0000_0010;
         }
 
         if self.will_flag {
-            flags_byte |= 0b0000_0100; // Setea el bit 2 (Will Flag)
-            flags_byte |= (self.will_qos << 3) & 0b0001_1000; // Ajusta los bits 3 y 4 (Will QoS)
+            flags_byte |= 0b0000_0100;
+            flags_byte |= (self.will_qos << 3) & 0b0001_1000;
             if self.will_retain {
-                flags_byte |= 0b0010_0000; // Setea el bit 5 (Will Retain)
+                flags_byte |= 0b0010_0000; 
             }
         }
 
         if self.user_name_flag {
-            flags_byte |= 0b1000_0000; // Setea el bit 7 (User Name Flag)
+            flags_byte |= 0b1000_0000; 
         }
 
         if self.password_flag {
-            flags_byte |= 0b0100_0000; // Setea el bit 6 (Password Flag)
+            flags_byte |= 0b0100_0000; 
         }
 
         flags_byte
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-// Importa las bibliotecas necesarias
 use log::{info, warn, error};
 
 fn main() {
-    // Configura el sistema de registro
     env_logger::init();
 
-    // Ejemplo de uso de registros
     info!("Este es un mensaje de información.");
     warn!("Esto es una advertencia.");
     error!("Esto es un error crítico.");
