7bc091b849df2c26094dc24ea14ff153bbb7fb29
ArelyAgama <aagama@fi.uba.ar>
2024-04-24 00:26:59 -0300

    add flags

---
+++ src/subscribe-flags.rs
@@ -0,0 +1,17 @@
+//struct para manejar los flags de subscribe mqtt
+#[derive(Debug)]
+pub struct SubscribeFlags {
+    pub qos: u8,
+}
+
+impl SubscribeFlags {
+    pub fn new(qos: u8) -> Self {
+        SubscribeFlags {
+            qos,
+        }
+    }
+    /// Convierte los flags de subscribe en un byte
+    pub fn to_flags_byte(&self) -> u8 {
+        self.qos & 0b0000_0011 // Solo se toman los 2 bits menos signif, dado que qos solo puede ser 0, 1 o 2
+    }
+}
