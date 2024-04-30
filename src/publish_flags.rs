48bd04eb85d79219667e25e7c964cbe4d1821a44
Aldana <aldanuchi@gmail.com>
2024-04-30 10:11:45 -0300

    (fix clippy).

--- src/publish_flags.rs
+++ src/publish_flags.rs
@@ -9,6 +9,13 @@ impl PublishFlags {
         PublishFlags {  }
     }
 }
+
+// Temporal. Esto es para que no se queje el clippy.
+impl Default for PublishFlags {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 impl PublishFlags {
     pub fn to_bytes(&self) -> Vec<u8> { // [] ver
         vec![0] // probando, para que compile
