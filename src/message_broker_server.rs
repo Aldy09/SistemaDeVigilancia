c9be4f6c8d686e05a7f9851a52cf5e9446ebf308
Aldana <aldanuchi@gmail.com>
2024-05-06 05:49:41 -0300

    (Hago que devuelvan result para usar signo de pregunta).

--- src/message_broker_server.rs
+++ src/message_broker_server.rs
@@ -2,79 +2,74 @@ use config::{Config, File, FileFormat};
 use log::info;
 use rustx::connect_message::ConnectMessage;
 use rustx::fixed_header::{FixedHeader, self};
-use std::io::{Read, Write};
+use std::io::{Read, Write, Error};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
 
     const FIXED_HEADER_LEN: usize = FixedHeader::fixed_header_len();
     let mut fixed_header_buf: [u8; 2] = [0; FIXED_HEADER_LEN];
 
-    let res = stream.read(&mut fixed_header_buf);
-    match res {
-        Ok(_) => {
-            // He leído bytes de un fixed_header, tengo que ver de qué tipo es.
-            let fixed_header = FixedHeader::from_bytes(fixed_header_buf.to_vec());
-            let tipo = fixed_header.get_tipo();
-            println!("Recibo msj con tipo: {}, bytes de fixed header leidos: {:?}", tipo, fixed_header);
-            match tipo {
-                1 => { // Es tipo Connect, acá procesar el connect y dar connack
-                    println!("Recibo mensaje tipo Connect");
-                    // Instancio un buffer para leer los bytes restantes, siguientes a los de fixed header
-                    let msg_rem_len: usize = fixed_header.get_rem_len();
-                    let mut rem_buf = vec![0; msg_rem_len];
-                    let _res = stream.read(&mut rem_buf).expect("Error al leer mensaje");
-
-                    // Ahora junto las dos partes leídas, para obt mi msg original
-                    let mut buf = fixed_header_buf.to_vec();
-                    buf.extend(rem_buf);
-                    // Entonces tengo el mensaje completo
-                    let connect_msg = ConnectMessage::from_bytes(&buf);
-                    println!("   Mensaje connect completo recibido: {:?}", connect_msg);
-
-
-
-                    let username = "sistema-monitoreo";
-                    let password = "rutx123";
-                    let is_authentic: bool = buf
-                        .windows(username.len() + password.len() + 2)
-                        .any(|slice| slice == [username.as_bytes(), password.as_bytes(), &[0x00]].concat());
-
-                    let connack_response: [u8; 4] = if is_authentic {
-                        [0x20, 0x02, 0x00, 0x00] // CONNACK (0x20) con retorno 0x00
-                    } else {
-                        [0x20, 0x02, 0x00, 0x05] // CONNACK (0x20) con retorno 0x05 (Refused, not authorized)
-                    };
-                    stream
-                        .write_all(&connack_response)
-                        .expect("Error al enviar CONNACK");
-
-                },
-                3 => { // Es Publish
-
-
-
-                },
-                8 => { // Es Subscribe
-
-                },
-                _ => println!("Error, tipo de mensaje recibido no identificado."),
-
-            }
-        },
-        Err(e) => println!("Error al leer el los bytes del fixed header por el socket."),
-    }
-
-
-
+    let res = stream.read(&mut fixed_header_buf)?;
     
+    // He leído bytes de un fixed_header, tengo que ver de qué tipo es.
+    let fixed_header = FixedHeader::from_bytes(fixed_header_buf.to_vec());
+    let tipo = fixed_header.get_tipo();
+    println!("Recibo msj con tipo: {}, bytes de fixed header leidos: {:?}", tipo, fixed_header);
+    match tipo {
+        1 => { // Es tipo Connect, acá procesar el connect y dar connack
+            println!("Recibo mensaje tipo Connect");
+            // Instancio un buffer para leer los bytes restantes, siguientes a los de fixed header
+            let msg_rem_len: usize = fixed_header.get_rem_len();
+            let mut rem_buf = vec![0; msg_rem_len];
+            let _res = stream.read(&mut rem_buf).expect("Error al leer mensaje");
 
-    // 
-    //stream.read(&mut buf).unwrap(); // Aux probando []
-    //println!("DESDE SERVER, LEO: {:?}", buf); // (veo manualmente que sí me llegó el struct), en construcción
+            // Ahora junto las dos partes leídas, para obt mi msg original
+            let mut buf = fixed_header_buf.to_vec();
+            buf.extend(rem_buf);
+            // Entonces tengo el mensaje completo
+            let connect_msg = ConnectMessage::from_bytes(&buf);
+            println!("   Mensaje connect completo recibido: {:?}", connect_msg);
+
+            // Se fija si la conexión se establece correctamente y responde un connack
+            let username = "sistema-monitoreo";
+            let password = "rutx123";
+            let is_authentic: bool = buf
+                .windows(username.len() + password.len() + 2)
+                .any(|slice| slice == [username.as_bytes(), password.as_bytes(), &[0x00]].concat());
+
+            let connack_response: [u8; 4] = if is_authentic {
+                [0x20, 0x02, 0x00, 0x00] // CONNACK (0x20) con retorno 0x00
+            } else {
+                [0x20, 0x02, 0x00, 0x05] // CONNACK (0x20) con retorno 0x05 (Refused, not authorized)
+            };
+            stream
+                .write_all(&connack_response)
+                .expect("Error al enviar CONNACK");
+
+            if is_authentic {
+                // A partir de ahora que ya se hizo el connect exitosamente,
+                // se puede empezar a recibir publish y subscribe de ese cliente.
+                continuar_la_conexion(&mut stream)?;
+            };
+
+        },
+        _ => {
+            println!("Error, el primer mensaje recibido DEBE ser un connect.");
+            // ToDo: Leer de la doc qué hacer en este caso, o si solo ignoramos.
+        },
+    };
+    Ok(())
 }
 
-fn main() {
+/// Se ejecuta una vez recibido un `ConnectMessage` exitoso y devuelto un `ConnAckMessage` acorde.
+/// Se puede empezar a recibir mensajes de otros tipos (`Publish`, `Subscribe`), de este cliente.
+/// Recibe el `stream` para la comunicación con el cliente en cuestión.
+fn continuar_la_conexion(stream: &mut TcpStream) -> Result<(), Error> {
+    Ok(())
+}
+
+fn main() -> Result<(), Error>{
     env_logger::init();
 
     info!("Leyendo archivo de configuración.");
@@ -114,4 +109,6 @@ fn main() {
             eprintln!("Error al esperar el hilo: {:?}", e);
         }
     }
+
+    Ok(())
 }
