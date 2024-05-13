6edaf777790664d0fabd2619f24c537c391e87aa
Aldana <aldanuchi@gmail.com>
2024-05-12 22:36:16 -0300

    Resultado del meet de hoy, funciona la superposición, con varias imágenes :).

--- src/sistema_monitoreo.rs
+++ src/sistema_monitoreo.rs
@@ -63,22 +63,43 @@ fn build_ui(application: &gtk::Application) {
     menubar.append(&item_incidents);
     menubar.append(&item_exit);
 
+    // Un layout para el menubar y la estructura de imágenes superpuestas
     let layout = gtk::Box::new(gtk::Orientation::Vertical, 0);
     layout.pack_start(&menubar, false, false, 0);
 
+    // Se crea un overlay para permitir superposición de imágenes
     let overlay = gtk::Overlay::new();
     layout.pack_start(&overlay, true, true, 0);
 
+    // Imagen base
     let map_image = gtk::Image::from_file("mapa_temp.png");
     overlay.add(&map_image);
 
-    let superimposed_image = gtk::Image::from_file("dron.png");
+    // Imagen superpuesta: dron
+    let superimposed_image = gtk::Image::from_file("dron_70.png");
     overlay.add_overlay(&superimposed_image);
     gtk::WidgetExt::set_halign(&superimposed_image, gtk::Align::Start);
     gtk::WidgetExt::set_valign(&superimposed_image, gtk::Align::Start);
-    gtk::WidgetExt::set_margin_start(&superimposed_image, 0);
-    gtk::WidgetExt::set_margin_top(&superimposed_image, 0);
+    gtk::WidgetExt::set_margin_start(&superimposed_image, 100);
+    gtk::WidgetExt::set_margin_top(&superimposed_image, 100);
     overlay.set_child_index(&superimposed_image, 0);
+
+    let dron2 = gtk::Image::from_file("dron_70.png");
+    overlay.add_overlay(&dron2.clone());
+    gtk::WidgetExt::set_halign(&dron2, gtk::Align::Start);
+    gtk::WidgetExt::set_valign(&dron2, gtk::Align::Start);
+    gtk::WidgetExt::set_margin_start(&dron2, 200);
+    gtk::WidgetExt::set_margin_top(&dron2, 300);
+    overlay.set_child_index(&dron2, 0);
+
+    // Imagen superpuesta: cámara
+    let cam_img = gtk::Image::from_file("camara_50.png");
+    overlay.add_overlay(&cam_img);
+    gtk::WidgetExt::set_halign(&cam_img, gtk::Align::Start);
+    gtk::WidgetExt::set_valign(&cam_img, gtk::Align::Start);
+    gtk::WidgetExt::set_margin_start(&cam_img, 600);
+    gtk::WidgetExt::set_margin_top(&cam_img, 400);
+    overlay.set_child_index(&cam_img, 0);
 
     window.add(&layout);
     window.show_all();
