c96774a10e0f4f32625ccef6845b49c8710b6ea9
Aldana <aldanuchi@gmail.com>
2024-05-15 06:19:09 -0300

    Modificando uso desde afuera por parte de un cliente (sin spawn afuera).

--- src/message_broker_client.rs
+++ src/message_broker_client.rs
@@ -35,15 +35,32 @@ fn main() {
             //info!("Conectado al broker MQTT."); //
             println!("Cliente: Conectado al broker MQTT.");
 
+            // Cliente usa subscribe
+            let res_sub = mqtt_client.mqtt_subscribe(1, vec![(String::from("topic3"), 1)]);
+            match res_sub {
+                Ok(_) => println!("Cliente: Hecho un subscribe exitosamente"),
+                Err(e) => println!("Cliente: Error al hacer un subscribe: {:?}", e),
+            }
+
+            // Cliente usa publish
+            let res = mqtt_client.mqtt_publish("topic3", "hola mundo :)".as_bytes());
+            match res {
+                Ok(_) => println!("Cliente: Hecho un publish exitosamente"),
+                Err(e) => error!("Cliente: Error al hacer el publish {:?}", e),
+            }
+
+            // Acá falta que lea del topic del cual hizo subscribe, por implementar [].
+
+            // Cliente termina de utilizar mqtt
+            mqtt_client.finalizar();
+
+            ////////////////
+            // Todo esto de acá abajo no, ya no hay hilos acá afuera sino que hay spawn adentro.
+
             //let mut mqtt_client_para_hijo = mqtt_client.mqtt_clone();
 
-            let h_pub = thread::spawn(move || {
-                // Cliente usa publish
-                let res = mqtt_client_para_hijo.mqtt_publish("topic3", "hola mundo :)".as_bytes());
-                match res {
-                    Ok(_) => println!("Cliente: Hecho un publish exitosamente"),
-                    Err(e) => error!("Cliente: Error al hacer el publish {:?}", e),
-                }
+            /*let h_pub = thread::spawn(move || {
+                
 
                 //println!("Cliente: fin publish1, viene publish2");
                 /*// Emulo que Cliente haga un segundo publish, al mismo topic
@@ -52,19 +69,11 @@ fn main() {
                     Ok(_) => println!("Cliente: Hecho un publish exitosamente"),
                     Err(e) => error!("Cliente: Error al hacer el publish {:?}", e),
                 }*/
-            });
+            });*/
 
-            let h_sub = thread::spawn(move || {
+            /*let h_sub = thread::spawn(move || {
                 // Cliente usa subscribe // Aux: me suscribo al mismo topic al cual el otro hilo está publicando, para probar
-                let res_sub = mqtt_client.mqtt_subscribe(1, vec![(String::from("topic3"), 1)]);
-                match res_sub {
-                    Ok(_) => {
-                        println!("Cliente: Hecho un subscribe exitosamente");
-                    }
-                    Err(e) => {
-                        println!("Cliente: Error al hacer un subscribe: {:?}", e);
-                    }
-                }
+                
                 // Inicio Probando
                 // Yo voy a leer del stream, (qué espero leer?), xq ya hice el subscribe.
                 // Es decir ya le envié a server un msg tipo Subscribe.
@@ -90,15 +99,15 @@ fn main() {
                     println!("[loop cliente subscribe] vuelta leí bytes: {:?}", bytes_leidos);
                     veces -= 1;
                 }*/
-            });
+            });*/
 
-            // Esperar a los hijos
+            /*// Esperar a los hijos
             if h_pub.join().is_err() {
                 println!("Error al esperar a hijo publisher.");
             }
             if h_sub.join().is_err() {
                 println!("Error al esperar a hijo subscriber.");
-            }
+            }*/
         }
         Err(e) => error!("Cliente: Error al conectar al broker MQTT: {:?}", e),
     }
