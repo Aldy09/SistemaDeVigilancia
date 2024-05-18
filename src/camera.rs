5e2121c1a7c6de3129c5b971d8a8870ac2518b9e
Martin <mmorilla@fi.uba.ar>
2024-05-18 19:58:08 -0300

    se agrega la funcion to_bytes y from_bytes al archivo de camera.rs

--- src/camera.rs
+++ src/camera.rs
@@ -21,6 +21,42 @@ impl Camera {
         }
     }
 
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend_from_slice(&self.id.to_be_bytes());
+        bytes.extend_from_slice(&self.coord_x.to_be_bytes());
+        bytes.extend_from_slice(&self.coord_y.to_be_bytes());
+        bytes.extend_from_slice(&self.state.to_byte());
+        bytes.extend_from_slice(&self.range.to_be_bytes());
+        bytes.extend_from_slice(&(self.border_cameras.len() as u8).to_be_bytes());
+        for camera in &self.border_cameras {
+            bytes.extend_from_slice(&camera.to_be_bytes());
+        }
+        bytes
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let id = u8::from_be_bytes([bytes[0]]);
+        let coord_x = i32::from_be_bytes([bytes[1], bytes[2], bytes[3], bytes[4]]);
+        let coord_y = i32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
+        let state = CameraState::from_byte([bytes[9]]);
+        let range = u8::from_be_bytes([bytes[10]]);
+        let border_cameras_len = u8::from_be_bytes([bytes[11]]);
+        let mut border_cameras = vec![];
+        for i in 0..border_cameras_len {
+            border_cameras.push(u8::from_be_bytes([bytes[12 + i as usize]]));
+        }
+        Self {
+            id,
+            coord_x,
+            coord_y,
+            state,
+            range,
+            border_cameras,
+        }
+    }
+
+
     pub fn display(&self) {
         println!("ID: {}", self.id);
         println!("Coordenada X: {}", self.coord_x);
