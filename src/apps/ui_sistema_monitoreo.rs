6aa2d746c8ff6b2eecfb488c02e5cadbb9deaaf3
Aldana <aldanuchi@gmail.com>
2024-06-09 21:54:22 -0300

    Saco cosas no usadas.

--- src/apps/ui_sistema_monitoreo.rs
+++ src/apps/ui_sistema_monitoreo.rs
@@ -26,12 +26,12 @@ pub enum Provider {
 
 fn http_options() -> HttpOptions {
     HttpOptions {
-        // Not sure where to put cache on Android, so it will be disabled for now.
-        cache: if cfg!(target_os = "android") || std::env::var("NO_HTTP_CACHE").is_ok() {
+        cache: None,
+        /*cache: if std::env::var("NO_HTTP_CACHE").is_ok() {
             None
         } else {
             Some(".cache".into())
-        },
+        },*/
         ..Default::default()
     }
 }
