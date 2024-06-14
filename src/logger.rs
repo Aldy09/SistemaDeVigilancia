cb47def597034e0e174c0de16d92c300056b39c5
Martin <mmorilla@fi.uba.ar>
2024-06-14 17:28:30 -0300

    fix cargo clippy

--- src/logger.rs
+++ src/logger.rs
@@ -18,7 +18,6 @@ impl Logger {
 
     pub fn write_in_file(&self, incident: Incident) {
         let mut file = std::fs::OpenOptions::new()
-            .write(true)
             .append(true)
             .create(true)
             .open("src/log.txt")
@@ -27,26 +26,5 @@ impl Logger {
         file.write_all(msg.as_bytes()).unwrap();
         
     }
-
-    // pub fn get_logger_rx(&self) -> Receiver<Incident> {
-    //     self.logger_rx
-    // }
-
-    // pub fn run(&self) {
-    //     loop {
-    //         while let Ok(msg) = self.logger_rx.recv() {
-    //             match msg {
-    //                 LogMessage::Info(msg) => {
-    //                     info!("{}", msg);
-    //                 }
-    //                 LogMessage::Warn(msg) => {
-    //                     warn!("{}", msg);
-    //                 }
-    //                 LogMessage::Error(msg) => {
-    //                     error!("{}", msg);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    
 }
