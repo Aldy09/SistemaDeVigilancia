0046f8f56d85230b5b088339cf05d600cacde120
Aldana <aldanuchi@gmail.com>
2024-06-20 07:53:11 -0300

    Dron: se agrega main de dron al cargo toml (antes no compilaba, es xq el name y el path tienen que llamarse igual, ahora sí).

---
+++ src/apps/dron_main.rs
@@ -0,0 +1,6 @@
+use rustx::apps::dron::Dron;
+
+fn main() {
+    let id = 1; // Aux: este id debería leerse de consola #ToDo [].
+    let _dron = Dron::new(id);
+}
--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -5,7 +5,6 @@ pub mod camera_state;
 pub mod common_client_errors;
 pub mod common_clients;
 pub mod dron;
-pub mod dron_app;
 pub mod dron_current_info;
 pub mod dron_flying_info;
 pub mod dron_state;
