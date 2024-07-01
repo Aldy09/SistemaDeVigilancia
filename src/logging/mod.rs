71e60fa7f58ab2cd2cc61a0fc9417c93370bc554
Aldana <aldanuchi@gmail.com>
2024-07-01 02:59:39 -0300

    Agrego archivo nuevo para mejorar el logger.

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,2 +1,3 @@
 pub mod logger;
+pub mod string_logger_writer;
 pub mod structs_to_save_in_logger;
---
+++ src/logging/string_logger_writer.rs
@@ -0,0 +1,20 @@
+use std::{io::{Error, Write}, sync::mpsc::Receiver};
+
+#[derive(Debug)]
+pub struct Logger {
+    pub logger_rx: Receiver<String>,
+}
+
+impl Logger {
+    pub fn new(logger_rx: Receiver<String>) -> Self {
+        Self { logger_rx }
+    }
+
+    pub fn write_to_file(&self, message: String) -> Result<(), Error> {
+        let mut file = std::fs::OpenOptions::new().append(true).open("s_log.txt")?;
+
+        writeln!(file, "{:?}\n", message)?;
+
+        Ok(())
+    }
+}
