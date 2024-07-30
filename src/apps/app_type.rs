fa25f2bf065b1eaf6364dde51f7ae9bb0896d723
Aldana <aldanuchi@gmail.com>
2024-07-30 05:51:07 -0300

    Se eliminan archivos que ya no se usaban.

--- src/apps/app_type.rs
+++
@@ -1,8 +0,0 @@
-use crate::apps::incident_data::incident::Incident;
-use crate::apps::sist_camaras::camera::Camera;
-
-#[derive(Debug)]
-pub enum AppType {
-    Camera(Camera),
-    Incident(Incident),
-}
